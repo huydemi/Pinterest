a24905c528668886c3fb3313a8313860ad4d16c7
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-29 22:04:45 +0700

    complete project

--- Pinterest/View Controllers/PhotoStreamViewController.swift
+++ Pinterest/View Controllers/PhotoStreamViewController.swift
@@ -35,36 +35,48 @@ class PhotoStreamViewController: UICollectionViewController {
   
   var photos = Photo.allPhotos()
   
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
+  override var preferredStatusBarStyle : UIStatusBarStyle {
+    return UIStatusBarStyle.lightContent
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+   
     if let patternImage = UIImage(named: "Pattern") {
       view.backgroundColor = UIColor(patternImage: patternImage)
     }
-    collectionView?.backgroundColor = .clear
-    collectionView?.contentInset = UIEdgeInsets(top: 23, left: 16, bottom: 10, right: 16)
+    collectionView?.backgroundColor = UIColor.clear
+    collectionView?.contentInset = UIEdgeInsets(top: 23, left: 10, bottom: 10, right: 10)
+    // Set the PinterestLayout delegate
+    if let layout = collectionView?.collectionViewLayout as? PinterestLayout {
+      layout.delegate = self
+    }
   }
   
 }
 
-extension PhotoStreamViewController: UICollectionViewDelegateFlowLayout {
+extension PhotoStreamViewController {
   
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return photos.count
   }
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AnnotatedPhotoCell", for: indexPath as IndexPath) as! AnnotatedPhotoCell
-    cell.photo = photos[indexPath.item]
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AnnotatedPhotoCell", for: indexPath)
+    if let annotateCell = cell as? AnnotatedPhotoCell {
+      annotateCell.photo = photos[indexPath.item]
+    }
     return cell
   }
   
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    let itemSize = (collectionView.frame.width - (collectionView.contentInset.left + collectionView.contentInset.right + 10)) / 2
-    return CGSize(width: itemSize, height: itemSize)
-  }
+}
+
+//MARK: - PINTEREST LAYOUT DELEGATE
+extension PhotoStreamViewController : PinterestLayoutDelegate {
   
+  // 1. Returns the photo height
+  func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath:IndexPath) -> CGFloat {
+    return photos[indexPath.item].image.size.height
+  }
+
 }
